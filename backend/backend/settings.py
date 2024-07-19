fffe5b52d4ac3521173485a203bf8356deea914f
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-07-19 00:25:54 -0700

    update cors conf

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,13 +62,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     'corsheaders.middleware.CorsMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware'
+     'whitenoise.middleware.WhiteNoiseMiddleware',
+         "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -111,6 +111,11 @@ CORS_ALLOW_METHODS = [
     'POST',
     'PUT',
 ]
+CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOWED_ORIGIN_REGEXES = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
+
+
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
